3711c1c708a724fe28f6b4d3ffa18fc2c8071489
Davit <xxxhellerxxx@gmail.com>
2019-12-07 00:11:40 +0400

    Add project files.

---
+++ wamshale_consoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wamshale_consoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+
+            Console.ReadLine();
+        }
+    }
+}
